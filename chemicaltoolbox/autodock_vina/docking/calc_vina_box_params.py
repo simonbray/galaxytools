e3af2d948218ffa0a1160e43bd379bc4110e5348
Simon Bray <sbray1371@gmail.com>
2019-04-16 10:50:58 +0200

    expand autodock vina

--- chemicaltoolbox/autodock_vina/docking/calc_vina_box_params.py
+++ chemicaltoolbox/autodock_vina/docking/calc_vina_box_params.py
@@ -9,16 +9,12 @@ import argparse
 
 
 def get_params(options):
-    # mol = Chem.MolFromMolFile('test-data/NUDT5A-x0114_2.mol')
     mol = Chem.MolFromMolFile(options.ligand_path)
     if not mol:
         raise IOError
 
-    print('test')
     conf = mol.GetConformer()
     params = rdShapeHelpers.ComputeConfBox(conf)
-
-    # mol = Chem.MolFromSdfFile(ligand)
 
 
     coords1 = np.array(params[0])
@@ -27,13 +23,9 @@ def get_params(options):
     print(coords1)
     print(coords2)
 
-
     center = np.mean((coords1, coords2), axis=0)
 
-    box_dims = np.abs(coords1 - coords2)
-
-    print(center)
-    print(box_dims)
+    box_dims = np.abs(coords1 - coords2) + (options.tolerance,) * 3
 
     with open(options.output, 'w') as f:
         f.write(
@@ -46,18 +38,19 @@ center_y =  {}
 center_z =  {}
 num_modes = 9999
 energy_range = 9999
-exhaustiveness = 10
+exhaustiveness = {}
 cpu = 4
 seed = 1
-            """.format(box_dims[0], box_dims[1], box_dims[2], center[0], center[1], center[2])
+            """.format(box_dims[0], box_dims[1], box_dims[2], center[0], center[1], center[2], options.exhaustiveness)
         )
-
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--ligand', dest='ligand_path')
     parser.add_argument('--config', dest='output')
+    parser.add_argument('--tol', dest='tolerance', default=0, type=float)
+    parser.add_argument('--exh', dest='exhaustiveness', default=10, type=float)
 
     options = parser.parse_args()
     get_params(options)
